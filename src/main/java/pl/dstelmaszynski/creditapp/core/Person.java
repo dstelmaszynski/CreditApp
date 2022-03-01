a529f1d79f12d6b18ec8553fdf5846bf010b02f0
Dominik <stelmaszynski.dominik@gmail.com>
2022-03-01 14:48:14 +0100

    creating Person class

--- src/main/java/pl/dstelmaszynski/creditapp/core/Person.java
+++ src/main/java/pl/dstelmaszynski/creditapp/core/Person.java
@@ -1,4 +1,25 @@
 package pl.dstelmaszynski.creditapp.core;
 
 public class Person {
+    private final String name;
+    private final String lastName;
+    private final String mothersMaidenName;
+
+    public Person(String name, String lastName, String mothersMaidenName) {
+        this.name = name;
+        this.lastName = lastName;
+        this.mothersMaidenName = mothersMaidenName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMothersMaidenName() {
+        return mothersMaidenName;
+    }
 }
